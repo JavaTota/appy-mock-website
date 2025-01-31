db006a53524e1dead0e0867a48817041960a9d35
JavaTota <jomigone@gmail.com>
2025-01-31 09:42:58 +0100

    chore: few minor adjustment

--- appy mock website/script.js
+++ appy mock website/script.js
@@ -3,9 +3,9 @@ const hiddenList = document.getElementById("hiddenList");
 const hiddenUl = document.getElementById("hiddenUl");
 
 hiddenList.addEventListener("mouseover", () => {
-  hiddenUl.classList.toggle("hide");
+  hiddenUl.classList.toggle("display");
 });
 
 hiddenList.addEventListener("mouseout", () => {
-  hiddenUl.classList.toggle("hide");
+  hiddenUl.classList.toggle("display");
 });
